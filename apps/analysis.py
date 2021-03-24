111f3d23d450c25ad61be69977a6034d1b02e8b8
pratikraut3103 <pratikraut3103@gmailcom>
2021-03-25 01:47:26 +0530

    inital commit

--- apps/analysis.py
+++ apps/analysis.py
@@ -21,7 +21,7 @@ layout = html.Div([
                                 ])
 
                             ])
-                        ],style={'background-color':'#CCD7EA','width':"320px"})
+                        ],style={'background-color':'#CCD7EA',"width":"100%"})
                     ]),
                     html.Br(),
                     dbc.Row([
@@ -36,15 +36,77 @@ layout = html.Div([
 
                             ])
                         ],style={'background-color':'#CCD7EA'})
+                    ]),
+
+                    html.Br(),
+                    dbc.Row([
+                        dbc.Card([
+                            dbc.CardBody([
+                                dbc.Row([
+                                    html.H3(" Download Historic Data"),
+                                    html.Br(),
+                                    dcc.DatePickerRange(
+                                        start_date_placeholder_text="Start Period",
+                                        end_date_placeholder_text="End Period",
+                                        calendar_orientation='horizontal',
+                                    )
+                                ]),
+                                dbc.Row([
+                                    dbc.Button
+                                ]),
+                            ])
+                        ],style={'background-color':'#CCD7EA'})
                     ])
-                ]),
+                ],width={"size":3}),
+
+                html.Br(),
 
                 dbc.Col([
                     dbc.Row([
-                        html.Div([],id = "book")
+                        dbc.Card([
+                            dbc.CardBody([
+                                dbc.Row([
+                                    html.Div([
+                                        dcc.RadioItems(
+                                            id='book_select',
+                                            options=[
+                                                {'label': 'Call', 'value': 'ce'},
+                                                {'label': 'Puts', 'value': 'pe'},
+                                            ],
+                                            value='ce',
+                                            labelStyle={'display': 'inline-block',"margin-right": "10px"}
+                                        ),
+                                        dbc.Toast(
+                                            "Call selected",
+                                            id="call_toast",
+                                            header="Call Values",
+                                            is_open=False,
+                                            dismissable=True,
+                                            icon="danger",
+                                            # top: 66 positions the toast below the navbar
+                                            style={"position": "fixed", "top": 66, "right": 10, "width": 350},
+                                        ),
+                                        dbc.Toast(
+                                            "Put selected",
+                                            id="put_toast",
+                                            header="Put Values",
+                                            is_open=False,
+                                            dismissable=True,
+                                            icon="danger",
+                                            # top: 66 positions the toast below the navbar
+                                            style={"position": "fixed", "top": 66, "right": 10, "width": 350},
+                                        )
+                                    ]),
+                                ]),
+                                dbc.Row([
+                                    html.Div([],id = "book"),
+                                ])
+
+                            ])
+                        ],style={'background-color':'#CCD7EA'})
                     ])
-                ]),
-            ])
+                ],width={"size":9}),
+            ],no_gutters=False)
 ])
 
 class Data:
@@ -81,11 +143,38 @@ def recom(value):
     return dbc.Table.from_dataframe(df,style={'font-family':'nudista-web",Helvetica,Arial,sans-serif','color':'black'})
 
 @app.callback(Output(component_id='book',component_property='children'),
-            [Input(component_id='search_box',component_property='value')])
-def recom(value):
+            [Input(component_id='search_box',component_property='value'),
+             Input(component_id="book_select",component_property='value')])
+def recom(value,book_value):
     d = Data(value)
     df = d.gat_option()
     df = pd.DataFrame(df)
-    print(df)
-    print(type(df))
-    return dbc.Table.from_dataframe(df,style={'font-family':'nudista-web",Helvetica,Arial,sans-serif','color':'black'})
+    if book_value == "ce" :
+        df = df[0].tolist()[0]
+        df = df.drop(columns=["openInterest",	"impliedVolatility","inTheMoney"	,"contractSize"	,"currency"])
+        df.fillna(value=0,inplace=True)
+        df = df[df['volume'] != 0]
+    elif book_value == "pe" :
+        df = df[0].tolist()[1]
+        df = df.drop(columns=["openInterest",	"impliedVolatility","inTheMoney"	,"contractSize"	,"currency"])
+        df.fillna(value=0,inplace=True)
+        df = df[df['volume'] != 0]
+    return dbc.Table.from_dataframe(df,style={'font-family':'nudista-web",Helvetica,Arial,sans-serif','color':'black',"font-size":"15px"})
+
+@app.callback(
+    Output("call_toast", "is_open"),
+    [Input("book_select", "value")],
+)
+def open_toast(n):
+    if n=="ce":
+        return True
+    return False
+
+@app.callback(
+    Output("put_toast", "is_open"),
+    [Input("book_select", "value")],
+)
+def open_toast(n):
+    if n=="pe":
+        return True
+    return False
